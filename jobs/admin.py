c9bcb4b9a8685d988e18de02331f79940df8c268
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2019-02-09 00:08:01 -0200

    configurações do django admin para criação de usuarios

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,76 @@
+from django import forms
 from django.contrib import admin
+from .models import MyUser, Empresa, Candidato
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth import admin as auth_admin
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirmação de senha', widget=forms.PasswordInput)
+
+    class Meta:
+        model = MyUser
+        fields = ('email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 != password2:
+            raise forms.ValidationError("As senhas digitidas não correspondem")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = MyUser
+        fields = ('email', 'password', 'first_name', 'last_name', 'is_active', 'is_admin')
+
+    def clean_password(self):
+        return self.initial["password"]
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = auth_admin.AdminPasswordChangeForm
+    list_display = ('email', 'first_name', 'last_name', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    limited_fieldsets = (
+        (None, {'fields': ('email',)}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    readonly_fields = ('last_login',)
+    filter_horizontal = ('groups', 'user_permissions')
+
 
 # Register your models here.
+admin.site.register(MyUser, UserAdmin)
+admin.site.register(Empresa)
+admin.site.register(Candidato)
