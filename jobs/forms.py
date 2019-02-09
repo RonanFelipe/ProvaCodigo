0d8c15e3a42c1e7d54775244c15e9a64208efcf1
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2019-02-09 00:43:19 -0200

    form signup

---
+++ jobs/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=255, required=True)
+    last_name = forms.CharField(max_length=255, required=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
