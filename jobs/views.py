50a412b1be83980f5261093432d3f7870158d2ee
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2019-02-09 00:54:33 -0200

    signup view mail and password

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=email, password=raw_password)
+            login(request, user)
+            return redirect('home')
+        else:
+            form = SignUpForm()
+        return render(request, 'registration/signup.html', {'form': form})
