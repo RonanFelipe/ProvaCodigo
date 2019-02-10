a105317cecc768a709689e4ec8fe1a42cfe87c80
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2019-02-10 02:45:48 -0200

    customization with bulma.css

--- provaDeConceito/settings.py
+++ provaDeConceito/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join('static'), )
 AUTH_USER_MODEL = 'jobs.MyUser'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
