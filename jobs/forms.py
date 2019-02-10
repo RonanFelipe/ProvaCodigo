85788ae794e7c5408c7a64bb1656e86b5ade353a
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2019-02-09 23:04:47 -0200

    login template

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from .models import Empresa, Candidato
 
 User = get_user_model()
 
@@ -12,3 +13,18 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
+
+
+class SignUpFormEmpresa(UserCreationForm):
+    first_name = forms.CharField(max_length=255, required=True)
+    last_name = forms.CharField(max_length=255, required=True)
+    cnpj = forms.IntegerField(required=True)
+    atividade = forms.CharField(max_length=255, required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user
